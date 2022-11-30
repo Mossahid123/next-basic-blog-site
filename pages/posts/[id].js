9bcd9613513299eccd7569d46c42aae1027c7e3f
Mossahid123 <mossahidhossain309@gmail.comm>
2022-11-30 14:19:59 +0600

    imported getpost data function

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,7 +1,16 @@
 import Layout from '../../components/layout';
-
-
 import { getAllPostIds } from '../../lib/posts';
+
+import { getAllPostIds, getPostData } from '../../lib/posts';
+
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
 
 export async function getStaticPaths() {
   const paths = getAllPostIds();
