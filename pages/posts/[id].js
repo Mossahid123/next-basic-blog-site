dc7c42cac5a338bb688db7a3f349586651d21f1c
Mossahid123 <mossahidhossain309@gmail.comm>
2022-11-30 14:26:51 +0600

    checked all data

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,32 +1,31 @@
-import Layout from '../../components/layout';
-import { getAllPostIds } from '../../lib/posts';
+import Layout from '../../components/layout'
+import { getAllPostIds, getPostData } from '../../lib/posts'
 
-import { getAllPostIds, getPostData } from '../../lib/posts';
-
-export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
-  return {
-    props: {
-      postData,
-    },
-  };
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  )
 }
 
 export async function getStaticPaths() {
-  const paths = getAllPostIds();
+  const paths = getAllPostIds()
   return {
     paths,
-    fallback: false,
-  };
+    fallback: false
+  }
 }
 
-export default function Post() {
-  return <Layout>...</Layout>;
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
+  }
 }
-export async function getStaticPaths() {
-    // Return a list of possible value for id
-  }
-  
-  export async function getStaticProps({ params }) {
-    // Fetch necessary data for the blog post using params.id
-  }
