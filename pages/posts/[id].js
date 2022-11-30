e6391cf66572aa72026c1d5ab803ae178fdb829c
Mossahid123 <mossahidhossain309@gmail.comm>
2022-11-30 15:04:53 +0600

    imported head tag from next at dynamic post file

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,31 +1,39 @@
+import Head from 'next/head';
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 
 export default function Post({ postData }) {
-  return (
-    <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-    </Layout>
-  )
+    return (
+        <Layout>
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
+            {postData.title}
+            <br />
+            {postData.id}
+            <br />
+            {postData.date}
+            <br />
+            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        </Layout>
+    );
 }
 
 export async function getStaticPaths() {
-  const paths = getAllPostIds()
-  return {
-    paths,
-    fallback: false
-  }
+    const paths = getAllPostIds()
+    return {
+        paths,
+        fallback: false
+    }
 }
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id)
-  return {
-    props: {
-      postData
-    }
-  }
+    // Add the "await" keyword like this:
+    const postData = await getPostData(params.id);
+
+    return {
+        props: {
+            postData,
+        },
+    };
 }
